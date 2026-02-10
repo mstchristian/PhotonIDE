249bb9724b48ef41a70ddecad7cd8e7844c514ab
Christian Aceves <acevesrocha2009@gmail.com>
2026-02-10 15:32:02 +0100

    Add main.py entry point printing Hello World
    Add main.py as the application's main entry point. The file includes an Apache-2.0 license header and a __main__ guard that prints "Hello, World!" when executed.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Christian Aceves
+# Licensed under the Apache License, Version 2.0
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# main.py - Main entry point for the application
+
+if __name__ == "__main__":
+    print("Hello, World!")
