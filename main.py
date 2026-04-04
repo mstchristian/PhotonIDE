443769994989f3bb3e08fcc7ef08eb2118dc3741
Christian Aceves <acevesrocha2009@gmail.com>
2026-04-05 00:24:39 +0200

    Create project base

---
+++ main.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026 Christian Aceves. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+
+def main():
+    print("Hello PhotonIDE")
+    
+if __name__ == "__main__":
+    main()
